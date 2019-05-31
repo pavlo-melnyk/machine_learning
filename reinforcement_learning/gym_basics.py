5a0ddb0c136ecded0bb59cd4ff3c2cea6348dd94
unknown <pavlo.o.melnyk@gmail.com>
2019-06-01 01:55:48 +0800

    initial commit

---
+++ reinforcement_learning/gym_basics.py
@@ -0,0 +1,63 @@
+import gym
+import numpy as np 
+
+
+# 1) get our environment:
+env = gym.make('CartPole-v1')
+
+# the full list of envs here:
+# https://gym.openai.com/envs
+
+
+# 2) start an episode:
+
+# put the Agent in the start state
+s = env.reset() # also returns the state
+print('\nthe start state:', s)
+
+# what do these numbers mean?
+# https://github.com/openai/gym/wiki/CartPole-v0
+
+# Num	Observation	 	       Min	       Max
+# 
+# 0	    Cart Position	       -2.4	       2.4
+# 1	    Cart Velocity	       -Inf	       Inf
+# 2	    Pole Angle	           ~ -41.8°	   ~ 41.8°
+# 3	    Pole Velocity At Tip   -Inf	       Inf
+
+# we can get some useful info from the console as well:
+box = env.observation_space
+# print('\n', box)
+# box. # type in the console and click [Tab]
+
+# similar with the actions:
+actions = env.action_space
+
+
+# 3) play an episode:
+
+# observation, reward, done, info = env.step(action)
+
+# the info dictionary is typically ignored
+
+# let's play the game taking random actions;
+# this will end quickly, b/c random actions can't keep 
+# the pole up for long:
+avg_steps = 0
+print('\nplaying the game taking random actions...')
+for t in range(10000):
+	i = 0 # number of steps taken in an episode
+	# play an episode;
+	done = False
+	while not done:
+		observation, reward, done, _ = env.step(actions.sample())
+		i += 1
+	# reset the environment once the game is over:
+	env.reset()
+	avg_steps += (i - avg_steps)/(t+1)
+
+print('\navg num of steps:', int(avg_steps))
+
+
+
+
---
+++ reinforcement_learning/random_search.py
@@ -0,0 +1,93 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt 
+
+
+def select_action(w, obs):
+	return 1 if w.dot(obs) > 0 else 0
+
+
+def play_game(env, w, display=False):
+	''' Plays an episode. Returns the number of steps. '''
+	# go to the start state:
+	observation = env.reset()	
+	done = False
+	i = 0 # number of steps per episode
+	while not done:
+		if display:
+			# watch the episode:
+			try:
+				env.render()
+			except ImportError:
+				pass
+		# select and take an action:
+		action = select_action(w, observation)			
+		observation, reward, done, _ = env.step(action)
+		i += 1
+	return i
+
+
+def random_search(env, epochs=100, n_episodes=100, verbose=False):
+	''' Does a random search in the parameter
+	space for a linear model.
+	'''
+	observation = env.reset()
+
+	best_weights = None
+	best_length = 0
+
+	lengths = []
+	# random search:
+	for e in range(epochs):
+		# ramdomly initialize the parameters:
+		weights = np.random.randn(*observation.shape)
+		avg_length = 0
+
+		# play the game:
+		for t in range(n_episodes):
+			i = play_game(env, weights)			
+			# update the average:
+			avg_length += (i - avg_length)/(t+1)
+		lengths.append(avg_length)
+
+		# update the weights:
+		if avg_length > best_length:
+			best_length = avg_length
+			best_weights = weights
+
+		if verbose and e % 5 == 0:
+			print('epoch:', e, '\tavg length:', avg_length)
+
+	return lengths, best_weights		
+
+
+if __name__ == '__main__':
+	# get the environment:
+	env = gym.make('CartPole-v1')
+
+	# parameters for the random search:
+	epochs = 100
+	n_episodes = 1000
+
+	# perform the random search:
+	lengths, best_weights = random_search(env, epochs, n_episodes, verbose=True)
+
+	print('\nbest avg length:', max(lengths))
+	print('\nbest weights:', best_weights)
+
+	plt.plot(lengths)
+	plt.title('Average Episode Length')
+	plt.xlabel('epochs')
+	plt.show()
+
+	# play again with the best found weights:
+	avg_length = 0
+	for t in range(n_episodes):
+		# display only the last episode;
+		i = play_game(env, best_weights, t==(n_episodes-1))
+		avg_length += (i - avg_length)/(t+1)
+
+	env.close()
+
+	print('\nfinal avg length:', avg_length)
+
