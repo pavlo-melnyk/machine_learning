021440636c7f270f93dd74446711de5a4410e30d
unknown <pavlo.o.melnyk@gmail.com>
2020-01-19 18:00:00 +0100

    minor updates

--- linear_regression/L2_regularization.py
+++ linear_regression/L2_regularization.py
@@ -1,17 +1,22 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+
 # Let's generate some data:
-N = 50 
+N = 23 
 X = np.linspace(0, 10, N)
 Y = 0.5*X + np.random.randn(N)
+n_outliers = 3
 
 # We are going to mannually make some outliers, 
-# so set the last point to 30 bigger, etc.
-Y[-1] += 30
-Y[-2] += 30
+# so set the last point to 10 bigger, etc.
+Y[-n_outliers:] += 10
 
 plt.scatter(X, Y)
+plt.xlabel('size')
+plt.ylabel('price')
+plt.title('Product Price as a (Linear) Function of Size') 
+plt.grid()
 plt.show()
 
 # add the bias term:
@@ -24,17 +29,43 @@ Y_ml = X.dot(w_ml) # predicted values
 # plot the solution (X without the bias column)
 plt.scatter(X[:,1], Y)
 plt.plot(X[:,1], Y_ml, label='maximum likelihood line', color='r')
-plt.legend() 
+plt.xlabel('size')
+plt.ylabel('price')
+plt.legend()
+plt.title('Product Price as a (Linear) Function of Size')
+plt.grid() 
 plt.show()
 
+# then calculate max likelihood solution 
+# for the same data but without outliers:
+X_cut = X[:-n_outliers]
+Y_cut = Y[:-n_outliers]
 
-# Now let's apply L2-regularisation (Ridge Regression):
+w_ml_2 = np.linalg.solve(X_cut.T.dot(X_cut), X_cut.T.dot(Y_cut))
+Y_ml_2 = X.dot(w_ml_2)
+
+plt.scatter(X[:,1], Y)
+plt.plot(X[:,1], Y_ml, label='maximum likelihood line', color='r')
+plt.plot(X[:,1], Y_ml_2, label='main trend', color='g')
+plt.xlabel('size')
+plt.ylabel('price')
+plt.legend()
+plt.title('Product Price as a (Linear) Function of Size')
+plt.grid() 
+plt.show()
+
+# Now let's apply L2-regularization (Ridge Regression):
 lmd = 1000
 w_reg = np.linalg.solve(((X.T.dot(X)) + lmd*np.eye(2)), X.T.dot(Y))
 Y_reg = X.dot(w_reg)
 
 plt.scatter(X[:,1], Y)
 plt.plot(X[:,1], Y_ml, label='maximum likelihood line', color='r')
-plt.plot(X[:,1], Y_reg, label='regularised prediction', color='g')
-plt.legend() 
+plt.plot(X[:,1], Y_ml_2, label='main trend', color='g')
+plt.plot(X[:,1], Y_reg, label='regularized prediction', color='b')
+plt.xlabel('size')
+plt.ylabel('price')
+plt.legend()
+plt.title('Product Price as a (Linear) Function of Size')
+plt.grid() 
 plt.show()
